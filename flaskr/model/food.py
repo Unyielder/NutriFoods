1606ec126af1d02e8b3487573692cbe807f72101
Mahim Temacini <mo.temacini@gmail.com>
2020-11-08 18:09:21 -0500

    finished macro query and added change_serving function

--- flaskr/model/food.py
+++ flaskr/model/food.py
@@ -1,15 +1,23 @@
 class Food:
-    def __init__(self, food_name, measure, unit, grams, calories, proteins,
-                 fat, sat_fat, fiber, carbs, category):
+    def __init__(self, food_name, measure, unit, calories, proteins,
+                 fat, sat_fat, fiber, carbs):
         self.food_name = food_name
         self.measure = measure
         self.unit = unit
-        self.grams = grams
         self.calories = calories
         self.proteins = proteins
         self.fat = fat
         self.sat_fat = sat_fat
         self.fiber = fiber
         self.carbs = carbs
-        self.category = category
+
+    def change_serving(self, factor):
+        self.measure = self.measure * factor
+        self.calories = self.calories * factor
+        self.proteins = self.proteins * factor
+        self.fat = self.fat * factor
+        self.sat_fat = self.sat_fat * factor
+        self.fiber = self.fiber * factor
+        self.carbs = self.carbs * factor
+
 
---
+++ flaskr/model/queries.py
@@ -0,0 +1,4 @@
+class Queries:
+    def __init__def(self):
+        self.ingredient_query = None
+        self.macro_query = None
