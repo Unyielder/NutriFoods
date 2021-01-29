162e4d7f4872722ea9141d3b9a029b0c70d32c45
Mahim Temacini <mo.temacini@gmail.com>
2021-01-28 22:45:01 -0500

    completed ingredient macro query

--- flaskr/model.py
+++ flaskr/model.py
@@ -1,12 +1,12 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, and_
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
 from flask_login import UserMixin
+
 from flaskr.db import db_session
 
-
-engine = create_engine(r"sqlite:///C:\Users\Mohamed\PycharmProjects\Meal Prep\instance\Canadian_Foods.db", convert_unicode=True, echo=False)
+engine = create_engine(r"sqlite:///C:\Users\Mohamed\PycharmProjects\Meal Prep\instance\Canadian_Foods.db",
+                       convert_unicode=True, echo=False)
 
 # reflect the tables
 Base = automap_base()
@@ -30,7 +30,7 @@ Base.query = db_session.query_property()
 
 
 class User(Base, UserMixin):
-    __tablename__= 'USER'
+    __tablename__ = 'USER'
 
     id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
     UserName = Column(String, unique=True, nullable=False)
@@ -41,75 +41,23 @@ class User(Base, UserMixin):
         return '<User %r>' % self.username
 
 
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
 
 if __name__ == '__main__':
 
-
-    """# testing addition of username and password in USERS table
-    db_session = scoped_session(sessionmaker(autocommit=False,
-                                             autoflush=False,
-                                             bind=engine))
     food_id = 2003
-    rs = db_session.query(FoodName.FoodID, FoodName.FoodDescription, MeasureName.MeasureDescription)\
-        .outerjoin(ConversionFactor, FoodName.FoodID == ConversionFactor.FoodID)\
-        .outerjoin(MeasureName, ConversionFactor.MeasureID == MeasureName.MeasureID)\
-        .filter(FoodName.FoodID == food_id).all()
+    rs = db_session.query(FoodName.FoodID, FoodName.FoodDescription, MeasureName.MeasureDescription,
+                          NutrientName.NutrientName,
+                          (ConversionFactor.ConversionFactorValue * NutrientAmount.NutrientValue).label(
+                              'NutrientValCalc'),
+                          NutrientName.NutrientUnit) \
+        .outerjoin(ConversionFactor, FoodName.FoodID == ConversionFactor.FoodID) \
+        .outerjoin(MeasureName, ConversionFactor.MeasureID == MeasureName.MeasureID) \
+        .outerjoin(NutrientAmount, FoodName.FoodID == NutrientAmount.FoodID) \
+        .outerjoin(NutrientName, NutrientAmount.NutrientID == NutrientName.NutrientID) \
+        .filter(and_(NutrientName.NutrientCode.in_((208, 203, 204, 606, 291, 205)),
+                     FoodName.FoodID == 2003, MeasureName.MeasureDescription == '100ml')).all()
+
     for row in rs:
         print(row)
 
-"""
-
-"""
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
-
-Base = declarative_base()
-Base.metadata.reflect(engine)
-
-
-class Users(Base):
-    __table__ = Base.metadata.tables['USERS']
-
-
-class MeasureName(Base):
-    __table__ = Base.metadata.tables['MEASURE_NAME']
-
-
-class ConversionFactor(Base):
-    __table__ = Base.metadata.tables['CONVERSION_FACTOR']
-
-
-class FoodGroup(Base):
-    __table__ = Base.metadata.tables['FOOD_GROUP']
-
-
-class FoodSource(Base):
-    __table__ = Base.metadata.tables['FOOD_SOURCE']
-
-
-class FoodName(Base):
-    __table__ = Base.metadata.tables['FOOD_NAME']
-    food_ids = relationship("YieldName", backref="FoodName")
-
-
-class YieldName(Base):
-    __table__ = Base.metadata.tables['YIELD_NAME']
-
-
-class YieldAmount(Base):
-    __table__ = Base.metadata.tables['YIELD_AMOUNT']
-
-
-class NutrientAmount(Base):
-    __table__ = Base.metadata.tables['NUTRIENT_AMOUNT']
-
-
-class NutrientName(Base):
-    __table__ = Base.metadata.tables['NUTRIENT_NAME']
-
-
-class NutrientSource(Base):
-    __table__ = Base.metadata.tables['NUTRIENT_SOURCE']
-
-"""
