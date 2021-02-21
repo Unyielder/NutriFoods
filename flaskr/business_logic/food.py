9e262727afdb19c523c1c88022f651eb91f6ed9e
Mahim Temacini <mo.temacini@gmail.com>
2021-02-21 14:07:46 -0500

    Listed all nutrients found in database and organized them in groups

--- flaskr/business_logic/food.py
+++ flaskr/business_logic/food.py
@@ -6,12 +6,29 @@ class Food:
         self.id = None
         self.name = ''
         self.measure = ''
+        # Macros
         self.calories = None
         self.proteins = None
         self.fat = None
         self.sat_fat = None
         self.fiber = None
         self.carbs = None
+        # Vitamins
+        self.thiamine = None # vitamin B1
+        self.vitamin_b12 = None
+        self.vitamin_b12_added = None
+        self.vitamin_b6 = None
+        self.niacin = None # form of vitamin B3
+        self.vitamin_c = None
+        self.vitamin_d_d2_d3 = None
+        self.vitamin_d = None
+        self.vitamin_d2 = None
+        self.vitamin_k = None
+        self.retinol = None
+        # Minerals
+        self.zinc = None
+        self.selenium = None
+
 
     def load_measure(self, result_set):
         self.measure = [res[2] for res in result_set]
