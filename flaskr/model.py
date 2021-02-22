693f37007cc5a0ad63df4846e1aecdb298840af4
Mahim Temacini <mo.temacini@gmail.com>
2021-02-22 00:43:30 -0500

    Pooled nutrient codes for vitamins, minerals, amino-acids, cholesterol and misc. To be implemented as queries at later commits

--- flaskr/model.py
+++ flaskr/model.py
@@ -45,18 +45,55 @@ class User(Base, UserMixin):
 
 if __name__ == '__main__':
 
-    food_id = 2003
+    food_id = 2
+
+    # Vitamins
+    nut_codes = (
+        430, 573, 323, 341, 342,
+        343, 325, 324, 328, 401,
+        435, 410, 415, 418, 578,
+        416, 404, 409, 406, 405,
+        417, 432, 431, 320, 319,
+        322)
+
+    # Minerals
+    nut_codes = (
+        301, 317, 306, 303, 315,
+        304, 312, 309
+    )
+
+    # Amino-acids
+    nut_codes = (
+        510, 509, 501, 502, 263,
+        517, 518, 508, 513, 514,
+        550, 504, 505, 503, 521,
+        512, 516, 515, 506, 507,
+        511, 203
+    )
+
+    # Cholesterol
+    nut_codes = (
+        639, 638, 641, 636, 601
+    )
+
+    # Misc
+    nut_codes = (
+        305, 245, 255, 337, 338,
+        421, 262, 454, 334, 321,
+        207, 221
+    )
+
     rs = db_session.query(FoodName.FoodID, FoodName.FoodDescription, MeasureName.MeasureDescription,
                           NutrientName.NutrientName,
-                          func.round((ConversionFactor.ConversionFactorValue * NutrientAmount.NutrientValue), 2).label('NutrientValCalc'),
+                          func.round((ConversionFactor.ConversionFactorValue * NutrientAmount.NutrientValue), 2).label(
+                              'NutrientValCalc'),
                           NutrientName.NutrientUnit) \
         .outerjoin(ConversionFactor, FoodName.FoodID == ConversionFactor.FoodID) \
         .outerjoin(MeasureName, ConversionFactor.MeasureID == MeasureName.MeasureID) \
         .outerjoin(NutrientAmount, FoodName.FoodID == NutrientAmount.FoodID) \
         .outerjoin(NutrientName, NutrientAmount.NutrientID == NutrientName.NutrientID) \
-        .filter(and_(NutrientName.NutrientCode.in_((208, 203, 204, 606, 291, 205)),
-                     FoodName.FoodID == 2003, MeasureName.MeasureDescription == '100ml')).all()
+        .filter(and_(NutrientName.NutrientCode.in_(nut_codes),
+                     FoodName.FoodID == 2, MeasureName.MeasureDescription == '100ml')).all()
 
     for row in rs:
         print(row)
-
